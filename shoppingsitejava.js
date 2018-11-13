bc6c7096a3c5eed71e5211d6b1fadf88667f22df
Lars Nihlmark <larsnihlmark@gmail.com>
2018-11-13 11:04:43 +0100

    Added delete button, removed calculator print out images

--- shoppingsitejava.js
+++ shoppingsitejava.js
@@ -4,14 +4,12 @@ function listLoaded() {
     
     cartProducts();
 
-
 }
 function cartProducts(){
     
     var productDiv = document.createElement("div");
     var mainProductAll = document.getElementById("allProducts");
-
-    console.log(cartItems)
+    productDiv.className = "allProducts";
     for(var i = 0; i < cartItems.length; i++) {
         var product = createCart(cartItems[i]);
         productDiv.appendChild(product);
@@ -19,29 +17,33 @@ function cartProducts(){
 
     mainProductAll.appendChild(productDiv);
 }
-/* function calculator() {
-    var numOfProducts = document.getElementById("cart1");
-    var stringcart = localStorage.getItem("shoppingcart");
-    var JsonCart = JSON.parse(stringcart);
-    numOfProducts.innerText = JsonCart.length;
-    
-} */
-function createCart(product){
-    console.log(product);
+
+function createCart(product){    
+
     var productContainer = document.createElement("div");
     productContainer.className = "product";
 
-    var productTitles = document.createElement("h1");
+    var productimages = document.createElement("img");
+    productimages.src = "./assets/" + product.image;
+    productContainer.appendChild(productimages);
+    
+    var productTitles = document.createElement("h2");
     productTitles.innerText = product.title;
     productContainer.appendChild(productTitles);
 
     var productPrices = document.createElement("h5");
     productPrices.innerText = product.price + "kr";
     productContainer.appendChild(productPrices);
+
+    var removeButton = document.createElement("button");
+    removeButton.className = "btn btn-danger";
+    var removeIcon = document.createElement("i");
+    removeIcon.className = "fas fa-trash-alt";
+    removeButton.appendChild(removeIcon);
+    var buttonText = document.createTextNode(" Ta bort");
+    removeButton.appendChild(buttonText);
+    productContainer.appendChild(removeButton);
     
-    var productimages = document.createElement("img");
-    productimages.src = "./assets/" + product.image;
-    productContainer.appendChild(productimages);
     
     return productContainer;
 }
