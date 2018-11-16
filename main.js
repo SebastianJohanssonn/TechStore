74d556b694b5a0aa24febccc5c944e2fa23face0
Sebastian Johansson <sebben.97@hotmail.com>
2018-11-16 08:39:56 +0100

    Added explanatory comments to each function.

--- main.js
+++ main.js
@@ -16,76 +16,64 @@ function loadProducts() {
 
 function initSite() {
     loadProducts();
-    // This would also be a good place to initialize other parts of the UI kjgjfg,
+    
 }
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
         var productDivs = document.createElement("div");
         productDivs.className = "productContainers";
-        for (var i = 0; i < listOfProducts.length; i++){
+        for (var i = 0; i < listOfProducts.length; i++) {
             var productInfo = createProductInfo(listOfProducts[i]);
             productDivs.appendChild(productInfo);      
         }
         
         document.querySelector("main").appendChild(productDivs);
+        
+}
+//Creating each individual phone.
+function createProductInfo(product) {
+    var productInfoContainer = document.createElement("div");
+    productInfoContainer.className = "productInfoContainer";
     
-        console.log(listOfProducts);
+    var getProductTitle = document.createElement("h1");
+    getProductTitle.innerText = product.title;
+    productInfoContainer.appendChild(getProductTitle);
+    
+    var getProductDescription = document.createElement("p");
+    getProductDescription.innerText = product.description;
+    productInfoContainer.appendChild(getProductDescription);
+    
+    var getProductImage = document.createElement("img");
+    getProductImage.src = "./assets/" + product.image;
+    productInfoContainer.appendChild(getProductImage);
         
-        // Add your code here, remember to brake your code in to smaller function blocks
-        // to reduce complexity and increase readability. Each function should have
-        // an explainetory comment like the one for this function, see row 22.
+    var getProductPrice = document.createElement("p");
+    getProductPrice.innerText = product.price + " kr";
+    productInfoContainer.appendChild(getProductPrice);
+    
+    var cartButton = document.createElement("button");
+    cartButton.className = "btn btn-primary";
+    cartButton.setAttribute("id", "addProduct");
+    cartButton.addEventListener("click", function() { addProduct(product) });
+    var cartIcon = document.createElement("i");
+    cartIcon.className = "cart2 fas fa-cart-arrow-down";
+    cartButton.appendChild(cartIcon);
+    var buttonText = document.createTextNode(" Lägg till i kundvagnen");
+    cartButton.appendChild(buttonText);
+    productInfoContainer.appendChild(cartButton);
         
-        // TODO: Remove the console.log and these comments when you've read them.
-    }
-
-    function createProductInfo(product){
-        var productInfoContainer = document.createElement("div");
-        productInfoContainer.className = "productInfoContainer";
-        
-        var getProductTitle = document.createElement("h1");
-        getProductTitle.innerText = product.title;
-        productInfoContainer.appendChild(getProductTitle);
-        
-        var getProductDescription = document.createElement("p");
-        getProductDescription.innerText = product.description;
-        productInfoContainer.appendChild(getProductDescription);
-        
-        var getProductImage = document.createElement("img");
-        getProductImage.src = "./assets/" + product.image;
-        productInfoContainer.appendChild(getProductImage);
-        
-        var getProductPrice = document.createElement("p");
-        getProductPrice.innerText = product.price + " kr";
-        productInfoContainer.appendChild(getProductPrice);
-
-        var cartButton = document.createElement("button");
-        cartButton.className = "btn btn-primary";
-        cartButton.setAttribute("id", "addProduct");
-        cartButton.addEventListener("click", function() { addProduct(product) });
-        var cartIcon = document.createElement("i");
-        cartIcon.className = "cart2 fas fa-cart-arrow-down";
-        cartButton.appendChild(cartIcon);
-        var buttonText = document.createTextNode(" Lägg till i kundvagnen");
-        cartButton.appendChild(buttonText);
-        productInfoContainer.appendChild(cartButton);
-        
-        return productInfoContainer;
-    }
-
-if (localStorage.shoppingcart){
+    return productInfoContainer;
+}
+//Parsing localstorage if there is one.
+if (localStorage.shoppingcart) {
     shoppingcart = JSON.parse(localStorage.shoppingcart);
 }
-
-function addProduct(product){
+//Adding phone to cart, array and saves it to localstorage.
+function addProduct(product) {
     var numberOfProducts = 1;
     document.getElementById("numberOfProducts").innerHTML = numberOfProducts + shoppingcart.length;
     shoppingcart.push(product);
     var shoppingcartString = JSON.stringify(shoppingcart);
     localStorage.shoppingcart = shoppingcartString;
     console.log(localStorage.shoppingcart);
-}
-
-
-
-
-    
+} 
